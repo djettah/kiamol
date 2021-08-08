bf5157339dd99930b0dc2a6a1dd66651d01e8f7d
Daniil Ovsyannikov <to.djet@gmail.com>
2021-08-08 10:36:35 +0300

    my solutions

--- ch14/loadgen.sh
+++ ch14/loadgen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-URL=$(kubectl get svc todo-proxy -o jsonpath='http://{.status.loadBalancer.ingress[0].*}:8015' -n kiamol-ch14-test)
+URL=$(kubectl get svc todo-proxy -o jsonpath='127.0.0.1:38084' -n kiamol-ch14-test)
 
-for i in {1..500}
+for i in {1..5000}
 do
    curl -s "$URL/List" > /dev/null
 done
---
+++ ch17/commands.sh
@@ -0,0 +1,2 @@
+kubectl patch clusterrolebinding docker-for-desktop-binding --type=json --patch '[{"op":"replace","path":"/subjects/0/name", "value":"system:serviceaccounts:kube-system"}]'
+
